1d90c091dc0976aa36b957f728f510185fa08181
JoelMndz <luisjo3lml@gmail.com>
2023-08-02 18:27:41 -0500

    reactive

--- vue-componentes/src/components/Formulario.vue
+++ vue-componentes/src/components/Formulario.vue
@@ -1,54 +1,52 @@
 <template>
   <div>
     <h1>Formulario</h1>
-    <input type="text" v-model="nombre">
-    <input type="date" v-model="fecha">
+    <input type="text" v-model="datos.nombre">
+    <input type="date" v-model="datos.fecha">
   </div>
-  <p>{{ nombre }} / {{ fecha }}</p>
   <div>
     <input 
-    v-model="aceptar" 
+    v-model="datos.aceptar" 
     id="terminos" 
     type="checkbox">
     <label for="terminos">Acepta los temrinos y condiciones</label>
   </div>
-  <pre>{{ aceptar }}</pre>
-  <div v-if="aceptar">
+  <div v-if="datos.aceptar">
     <div>
       <label for="A">Python</label>
       <input
-        v-model="lenguajes" 
+        v-model="datos.lenguajes" 
         type="checkbox" id="A" value="Python">
     </div>
     <div>
       <label for="Java">Java</label>
       <input
-        v-model="lenguajes" 
+        v-model="datos.lenguajes" 
         type="checkbox" id="Java" value="Java">
     </div>
-    {{ lenguajes }}
   </div>
   <div>
     <select 
-      v-model="pais"
+      v-model="datos.pais"
       name="pais" id="pais">
       <option disabled>Seleccione un país</option>
       <option :value="i" v-for="i in opciones" :key="i">
         {{ i }}
       </option>
     </select>
-    {{ pais }}
   </div>
+  <pre>{{ datos }}</pre>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const nombre = ref('')
-const fecha = ref('')
-const aceptar = ref(true)
-
-const lenguajes = ref(['Java'])
-const pais = ref('Peru')
+import { reactive, ref } from "vue";
+const datos = reactive({
+  nombre: '',
+  fecha:'',
+  lenguajes:[],
+  pais:'',
+  aceptar: true
+})
 const opciones = ref(['Ecuador','Chile','Mexico','España','Costa Rica','Peru'])
 </script>
 
