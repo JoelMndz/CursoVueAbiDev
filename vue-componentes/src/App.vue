935863d71da5fd384f29fcc9c29ad7fc27c25f94
JoelMndz <luisjo3lml@gmail.com>
2023-08-03 20:50:54 -0500

    v-for en un componente

--- vue-componentes/src/App.vue
+++ vue-componentes/src/App.vue
@@ -1,17 +1,38 @@
 <template>
-  <Formulario/>
+  <!-- <Formulario/>
   <Boton :nombre="nombreBOton" disabled class="success"/>
   <Boton type="submit" nombre="Eliminar" clase="danger"/>
-  <Boton type="submit" nombre="Cuidado!" clase="warning"/>
+  <Boton type="submit" nombre="Cuidado!" clase="warning"/> -->
+  <CardNoticia
+    v-for="noticia in listaNoticias"
+    :key="noticia.id"
+    :titulo="noticia.titulo"
+    :cuerpo="noticia.cuerpo"/>
 </template>
-
 <script setup lang="ts">
+import { ref } from 'vue';
 import Boton from './components/Boton.vue';
 import Formulario from './components/Formulario.vue';
+import CardNoticia from './components/CardNoticia.vue';
 const nombreBOton = "Enviar"
-  //PascalCase vs kebab-case
-  //import BotonContador from "./components/BotonContador.vue";
-  
+
+const listaNoticias = ref([
+  {
+    id: 1,
+    titulo: 'Nueva especie de mariposa descubierta en selva',
+    cuerpo: 'Científicos han anunciado el descubrimiento de una nueva especie de mariposa en la selva tropical. La mariposa tiene colores vibrantes y un patrón de alas único que ha intrigado a los expertos.',
+  },
+  {
+    id: 2,
+    titulo: 'Chocolate negro podría mejorar salud del corazón',
+    cuerpo: 'Un estudio reciente sugiere que el consumo moderado de chocolate negro podría tener efectos positivos en la salud del corazón. Los antioxidantes presentes en el chocolate negro podrían ayudar a reducir el riesgo de enfermedades cardíacas.',
+  },
+  {
+    id: 3,
+    titulo: 'Ruinas antiguas encontradas en el fondo del océano',
+    cuerpo: 'Mediante el uso de tecnología avanzada de exploración submarina, un equipo de científicos ha descubierto ruinas antiguas en el lecho marino. Estas ruinas podrían arrojar luz sobre civilizaciones perdidas de la historia.',
+  }
+]);
 </script>
 
 <style scoped>
---
+++ vue-componentes/src/components/CardNoticia.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">
+        {{ titulo }}
+      </h5>
+      <p class="card-text">
+        {{ cuerpo }}
+      </p>
+    </div>
+    <div class="card-footer">
+      <Boton 
+        nombre="Eliminar"
+        clase="danger"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Boton from './Boton.vue';
+
+
+defineProps({
+  titulo:{
+    type: String,
+    required: true
+  },
+  cuerpo:{
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style scoped>
+.card{
+  margin: 10px;
+}
+</style>
