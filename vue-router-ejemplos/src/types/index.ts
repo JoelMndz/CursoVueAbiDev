6513765e5223d2e2b8f21b55737c37b02bfaf3c3
JoelMndz <luisjo3lml@gmail.com>
2023-08-11 23:49:35 -0500

    Consumir api con fetch

---
+++ vue-router-ejemplos/src/types/index.ts
@@ -0,0 +1,7 @@
+export interface IUsuario{
+  id: number,
+  email: string,
+  first_name: string,
+  last_name: string,
+  avatar: string
+}
--- vue-router-ejemplos/src/views/UsuariosView.vue
+++ vue-router-ejemplos/src/views/UsuariosView.vue
@@ -1,19 +1,39 @@
 <template>
-  <h1>Usuarios</h1>
-  <button @click="contador++">{{ contador }}</button>
+  <div class="container">
+    <h1>Usuarios</h1>
+    <ul class="list-group">
+      <li 
+        class="list-group-item" 
+        v-for="usuario in usuarios" 
+        :key="usuario.id">
+        <RouterLink :to="`/usuario-detalle/${usuario.id}`">
+          {{ usuario.last_name }} {{ usuario.first_name }}
+
+        </RouterLink>
+      </li>
+    </ul>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 import {onBeforeMount, ref, onMounted, onBeforeUpdate, onUpdated, onBeforeUnmount, onUnmounted} from "vue";
+import type {IUsuario} from "@/types";
+import {RouterLink} from "vue-router";
+
+
 const contador = ref(0);
+const usuarios = ref<IUsuario[]>([])
 console.log('Created');
 
 onBeforeMount(()=>{
   console.log('onBeforeMount');
 })
 
-onMounted(()=>{
-  console.log('onMounted');
+onMounted(async()=>{
+  const respuesta = await fetch('https://reqres.in/api/users/')
+  const data = await respuesta.json()
+  usuarios.value = data.data  
 })
 
 onBeforeUpdate(()=>{
