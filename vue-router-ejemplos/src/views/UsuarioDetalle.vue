4ce3543e7879c58f5f7bf2c4c62f42b4e0f9ca68
JoelMndz <luisjo3lml@gmail.com>
2023-08-16 21:55:12 -0500

    useRouter

--- vue-router-ejemplos/src/views/UsuarioDetalle.vue
+++ vue-router-ejemplos/src/views/UsuarioDetalle.vue
@@ -6,17 +6,33 @@
       <img 
         :src="usuario?.avatar" alt="avatar">
     </div>
+    <div class="card-footer">
+      <button
+        class="btn btn-primary"
+        @click="regresar"
+      >Regresar</button>
+      <button
+        class="btn btn-primary ms-3"
+        @click="irHome"
+      >Ir al home</button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import {useRoute} from "vue-router";
+import {useRoute, useRouter} from "vue-router";
 import {onMounted, ref} from "vue";
 import type { IUsuario } from "@/types";
 
 const route = useRoute()
 const usuario = ref<IUsuario | null>(null)
-
+const router = useRouter()
+const regresar = ()=>{
+  router.back()
+}
+const irHome = ()=>{
+  router.push('/')
+}
 onMounted(async()=>{
   const id = route.params.usuarioId
   const respuesta = await fetch(`https://reqres.in/api/users/${id}`)
